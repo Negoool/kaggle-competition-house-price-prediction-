d0ae42a5e123a0d2f9ed2f4b38373ab49966e9b3
Negar Iravani <negariravani@Armans-MacBook-Pro.local>
2017-09-15 00:54:11 -0700

    first run on kaggle

--- p2.py
+++ p2.py
@@ -39,11 +39,17 @@ data = data_train.copy()
 # seperate X nad y
 y_train = data['SalePrice']
 X_train = data.drop(['Id','SalePrice'], axis = 1)
+X_test = data_test.drop(['Id'], axis =1)
+
+
+# delet outliers
 select_indice = (X_train['GrLivArea'] < 4000)
-print select_indice
 X_train = X_train[select_indice]
 y_train = y_train[select_indice]
-X_train.info()
+
+# this is how objectiv is defined
+y = y_train.values.reshape(-1,1)
+y_log = np.log1p(y)
 
 def modify_type(X):
     ''' some features are originally categorical but represented as numerical
@@ -73,6 +79,8 @@ def modify_type(X):
     })
     return X
 X_train = modify_type(X_train)
+X_test = modify_type(X_test)
+
 
 def cat_num_list(X):
     ''' seperate numerical and categorical features based on their type'''
@@ -139,14 +147,24 @@ class to_numpy(BaseEstimator, TransformerMixin):
         return X.values
 
 class my_OnehotEncoding(BaseEstimator, TransformerMixin):
-
+    ''' one hot encoding to change categorical data to numerical'''
     def fit(self, X_cat, y = None):
+        self.columns_list_ = list(pd.get_dummies(X_cat).columns)
         return self
     def transform(self, X_cat, y = None):
-        X_cat_1hot = pd.get_dummies(X_cat)
+        x_1hot_init = pd.get_dummies(X_cat)
+        if list(x_1hot_init.columns) == self.columns_list_:
+            X_cat_1hot = x_1hot_init
+        else:
+            exta_columns = Set(self.columns_list_) - Set(list(x_1hot_init.columns))
+            for c in exta_columns:
+                x_1hot_init[c] = 0
+            X_cat_1hot = x_1hot_init[self.columns_list_]
         return X_cat_1hot
 
+
 class feature_list(BaseEstimator, TransformerMixin):
+    ''' get the list of features for maybe later use'''
     def fit(self, X, y = None):
         self.attributes_ = X.columns
         return self
@@ -154,6 +172,7 @@ class feature_list(BaseEstimator, TransformerMixin):
         return X
 
 class skew_data(BaseEstimator, TransformerMixin):
+    ''' skew numerical data with skewness graeter than a limit'''
     def __init__(self, skewness_limit = .75):
         self.skewness_limit = skewness_limit
     def fit(self, X_num, y = None):
@@ -169,7 +188,7 @@ class skew_data(BaseEstimator, TransformerMixin):
 #
 ### handling missing data
 # number of missing values for each feature
-print X_train.isnull().sum(axis = 0).sort_values(ascending = False)
+# print X_train.isnull().sum(axis = 0).sort_values(ascending = False)
 # there are three groups
 # 1)not having a facility :custom_written class(none_to_category)
 # 2)real missing in numerical data: sklearn imputer
@@ -190,7 +209,6 @@ print cat_features
 att_none_to_0 = [x for x in  Set(att_missing_1).intersection(Set(num_features))]
 # meanssing value that None means NO(categirical )
 att_none_to_no = [x for x in  Set(att_missing_1).intersection(Set(cat_features))]
-
 
 
 num_pipeline = Pipeline([\
@@ -220,117 +238,143 @@ full_pipeline = FeatureUnion(transformer_list = [\
 ])
 #
 X_train_prepared = full_pipeline.fit_transform(X_train)
-
+X_test_prepared = full_pipeline.transform(X_test)
 #
 num_attributes = full_pipeline.transformer_list[0][1].steps[2][1].attributes_
 cat_attributes = full_pipeline.transformer_list[1][1].steps[4][1].attributes_
 total_attributes = list(num_attributes) + list(cat_attributes)
+
 #
 #
-y = y_train.values.reshape(-1,1)
-y_log = np.log1p(y)
-# plot learning curve: for unregulirized regression it is overfitted
-# a,b = [],[]
-# for N in np.arange(5, x_tr.shape[0], 100):
-#     lin_reg = LinearRegression(alpha = 2., random_state = 42)
-#     lin_reg.fit(x_tr[:N,:], y_tr[:N])
-#     predict_train = lin_reg.predict(x_tr[:N,:])
-#     a.append(np.sqrt(mean_squared_error(y_tr[:N] , predict_train)))
-#     b.append(np.sqrt(mean_squared_error(y_val , lin_reg.predict(x_val))))
-# plt.plot(a,'b')
-# plt.plot(b, 'r')
 
-from sklearn.linear_model import Ridge
-ridge = Ridge(random_state = 42, alpha = 16)
-# param_grid = {'alpha' : [15,16,17]}
-# grid = GridSearchCV(estimator = ridge, param_grid = param_grid,
-# scoring = 'neg_mean_squared_error', cv = 5, refit = True)
-# grid.fit(X_train_prepared , y_log)
-# for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
-#     print (np.sqrt(-score), param)
-# print grid.best_params_
-
-prediction_1 = cross_val_predict(ridge, X_train_prepared, y=y_log,
- cv=5)
-print np.sqrt(mean_squared_error(y_log, prediction_1))
-plt.figure()
-plt.plot(np.arange(len(y_log)), y_log, 'bo')
-
-plt.plot(np.arange(len(y_log)), prediction_1, 'go')
-
-
-# lasso = Lasso(random_state = 42)
-# param_grid = {'alpha' : [.0003,.0005,.0007,.0009]}
-# grid = GridSearchCV(estimator = lasso, param_grid = param_grid,
-# scoring = 'neg_mean_squared_error', cv = 5, refit = True)
-# grid.fit(X_train_prepared , y_log)
-# for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
-#     print (np.sqrt(-score), param)
-# print grid.best_params_
-
-lasso = Lasso(random_state = 42, alpha = .0005)
-prediction_2 = cross_val_predict(lasso, X_train_prepared, y=y_log,
- cv=5)
-print np.sqrt(mean_squared_error(y_log, prediction_2))
-plt.figure()
-plt.plot(np.arange(len(y_log)), y_log, 'bo')
-
-plt.plot(np.arange(len(y_log)), prediction_2, 'go')
-
-
-
+# # plot learning curve: for unregulirized regression it is overfitted
+# # a,b = [],[]
+# # for N in np.arange(5, x_tr.shape[0], 100):
+# #     lin_reg = LinearRegression(alpha = 2., random_state = 42)
+# #     lin_reg.fit(x_tr[:N,:], y_tr[:N])
+# #     predict_train = lin_reg.predict(x_tr[:N,:])
+# #     a.append(np.sqrt(mean_squared_error(y_tr[:N] , predict_train)))
+# #     b.append(np.sqrt(mean_squared_error(y_val , lin_reg.predict(x_val))))
+# # plt.plot(a,'b')
+# # plt.plot(b, 'r')
+#
+# from sklearn.linear_model import Ridge
+# ridge = Ridge(random_state = 42, alpha = 16)
+# # param_grid = {'alpha' : [15,16,17]}
+# # grid = GridSearchCV(estimator = ridge, param_grid = param_grid,
+# # scoring = 'neg_mean_squared_error', cv = 5, refit = True)
+# # grid.fit(X_train_prepared , y_log)
+# # for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
+# #     print (np.sqrt(-score), param)
+# # print grid.best_params_
+#
+# prediction_1 = cross_val_predict(ridge, X_train_prepared, y=y_log,
+#  cv=5)
+# print np.sqrt(mean_squared_error(y_log, prediction_1))
+# plt.figure()
+# plt.plot(np.arange(len(y_log)), y_log, 'bo')
+#
+# plt.plot(np.arange(len(y_log)), prediction_1, 'go')
+#
+#
+# # lasso = Lasso(random_state = 42)
+# # param_grid = {'alpha' : [.0003,.0005,.0007,.0009]}
+# # grid = GridSearchCV(estimator = lasso, param_grid = param_grid,
+# # scoring = 'neg_mean_squared_error', cv = 5, refit = True)
+# # grid.fit(X_train_prepared , y_log)
+# # for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
+# #     print (np.sqrt(-score), param)
+# # print grid.best_params_
+#
+# lasso = Lasso(random_state = 42, alpha = .0005)
+# prediction_2 = cross_val_predict(lasso, X_train_prepared, y=y_log,
+#  cv=5)
+# print np.sqrt(mean_squared_error(y_log, prediction_2))
+# plt.figure()
+# plt.plot(np.arange(len(y_log)), y_log, 'bo')
+#
+# plt.plot(np.arange(len(y_log)), prediction_2, 'go')
+#
+# lasso = Lasso(random_state = 42, alpha = .001)
+# prediction_2 = cross_val_predict(lasso, X_train_prepared, y=y_log,
+#  cv=5)
+# print np.sqrt(mean_squared_error(y_log, prediction_2))
+# lasso = Lasso(random_state = 42, alpha = .0005)
+# lasso.fit(X_train_prepared, y_log)
+# prediction_train_log = lasso.predict(X_train_prepared)
+# print np.sqrt(mean_squared_error(y_log, prediction_train_log))
+# prediction_test_log = lasso.predict(X_test_prepared)
+# prediction_test = np.exp(prediction_test_log) - 1
+# a = np.asarray(prediction_test)
+#np.savetxt("run1.csv", a, delimiter=",")
+#
+#
+#
+#
 # elastic = ElasticNet(random_state = 42,)
-# param_grid = {'alpha' : [.05,.1,.4,.8], 'l1_ratio': [0.05, .4,.8,1]}
+# param_grid = {'alpha' : [0004,.0003,.0004,.0009,.001], 'l1_ratio': [0.05, .8,1,1,2,1.5,1.7,2]}
 # grid = GridSearchCV(estimator = elastic, param_grid = param_grid,
 # scoring = 'neg_mean_squared_error', cv = 5, refit = True)
 # grid.fit(X_train_prepared , y_log)
 # for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
 #     print (np.sqrt(-score), param)
 # print grid.best_params_
-# l1 = .8 or .4, alpha = .05 0r .1 = 32000
-
-
-# rnd_reg = RandomForestRegressor(random_state = 42, n_jobs = -1)
-# param_grid = {'n_estimators' : [10,100,500]}
-# grid = GridSearchCV(estimator = rnd_reg, param_grid = param_grid,
-# scoring = 'neg_mean_squared_error', cv = 5, refit = True)
-# grid.fit(X_train_prepared , y)
-# for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
-#     print (np.sqrt(-score), param)
-# print grid.best_params_
-# for 500 & 100  = 30000
-##
-# rnd_reg = RandomForestRegressor(n_estimators= 100, random_state = 42, n_jobs = -1)
-# score_log = cross_val_score(rnd_reg, X_train_prepared, y=y_log,
-# scoring='neg_mean_squared_error', cv=5)
-# print np.sqrt(-score_log.mean())
-# rnd_reg.fit(X_train_prepared, y_log)
-# a = zip(rnd_reg.feature_importances_, total_attributes)
-# a.sort(key = lambda x :x[0], reverse = True)
-# for i in a:
-#     print i
-
-
-# grb_reg = GradientBoostingRegressor(random_state = 42)
-# param_grid = {'learning_rate' : [.05,.1,.5], 'n_estimators' :[10, 100, 500,1000]}
-# grid = GridSearchCV(estimator = grb_reg, param_grid = param_grid,
-# scoring = 'neg_mean_squared_error', cv = 5, refit = True)
-# grid.fit(X_train_prepared , y_log)
-# for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
-#     print (np.sqrt(-score), param)
-# print grid.best_params_
-##best result = (1000, .05), 25800 near to (500, .1) = 26200
-
-#
-grb_reg = GradientBoostingRegressor(random_state = 42, learning_rate = .05,
-n_estimators = 500)
-prediction_3 = cross_val_predict(grb_reg, X_train_prepared, y=y_log,
+# # alpha , l1_ratio = .0003, 1.7,
+elastic = ElasticNet(random_state = 42, alpha = .0003, l1_ratio = 1.7)
+prediction_2 = cross_val_predict(elastic, X_train_prepared, y=y_log,
  cv=5)
-print np.sqrt(mean_squared_error(y_log, prediction_3))
-plt.figure()
-plt.plot(np.arange(len(y_log)), y_log, 'bo')
-
-plt.plot(np.arange(len(y_log)), prediction_3, 'go')
-
-
-plt.show()
+print np.sqrt(mean_squared_error(y_log, prediction_2))
+elastic = ElasticNet(random_state = 42, alpha = .0003, l1_ratio = 1.7)
+elastic.fit(X_train_prepared, y_log)
+prediction_train_log = elastic.predict(X_train_prepared)
+print np.sqrt(mean_squared_error(y_log, prediction_train_log))
+prediction_test_log = elastic.predict(X_test_prepared)
+prediction_test = np.exp(prediction_test_log) - 1
+a = np.asarray(prediction_test)
+np.savetxt("run2.csv", a, delimiter=",")
+#
+#
+# # rnd_reg = RandomForestRegressor(random_state = 42, n_jobs = -1)
+# # param_grid = {'n_estimators' : [10,100,500]}
+# # grid = GridSearchCV(estimator = rnd_reg, param_grid = param_grid,
+# # scoring = 'neg_mean_squared_error', cv = 5, refit = True)
+# # grid.fit(X_train_prepared , y)
+# # for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
+# #     print (np.sqrt(-score), param)
+# # print grid.best_params_
+# # for 500 & 100  = 30000
+# ##
+# # rnd_reg = RandomForestRegressor(n_estimators= 100, random_state = 42, n_jobs = -1)
+# # score_log = cross_val_score(rnd_reg, X_train_prepared, y=y_log,
+# # scoring='neg_mean_squared_error', cv=5)
+# # print np.sqrt(-score_log.mean())
+# # rnd_reg.fit(X_train_prepared, y_log)
+# # a = zip(rnd_reg.feature_importances_, total_attributes)
+# # a.sort(key = lambda x :x[0], reverse = True)
+# # for i in a:
+# #     print i
+#
+#
+# # grb_reg = GradientBoostingRegressor(random_state = 42)
+# # param_grid = {'learning_rate' : [.05,.1,.5], 'n_estimators' :[10, 100, 500,1000]}
+# # grid = GridSearchCV(estimator = grb_reg, param_grid = param_grid,
+# # scoring = 'neg_mean_squared_error', cv = 5, refit = True)
+# # grid.fit(X_train_prepared , y_log)
+# # for score, param in zip(grid.cv_results_['mean_test_score'],grid.cv_results_['params']):
+# #     print (np.sqrt(-score), param)
+# # print grid.best_params_
+# ##best result = (1000, .05), 25800 near to (500, .1) = 26200
+#
+# #
+# grb_reg = GradientBoostingRegressor(random_state = 42, learning_rate = .05,
+# n_estimators = 500)
+# prediction_3 = cross_val_predict(grb_reg, X_train_prepared, y=y_log,
+#  cv=5)
+# print np.sqrt(mean_squared_error(y_log, prediction_3))
+# plt.figure()
+# plt.plot(np.arange(len(y_log)), y_log, 'bo')
+#
+# plt.plot(np.arange(len(y_log)), prediction_3, 'go')
+#
+#
+# plt.show()
